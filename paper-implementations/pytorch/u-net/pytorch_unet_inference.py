402f56a9efe2673c5c0f828ddbb8d97da6876a02
Asif Ahmed <public.quickgrid@gmail.com>
2021-10-26 21:46:48 +0600

    added samples

--- paper-implementations/pytorch/u-net/pytorch_unet_inference.py
+++ paper-implementations/pytorch/u-net/pytorch_unet_inference.py
@@ -1,9 +1,12 @@
+#import cv2
+from cv2 import cv2
 import numpy as np
 from PIL import Image
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 import torch
 import torchvision
+
 from pytorch_unet_train import UNET
 
 
@@ -14,6 +17,37 @@ IMAGE_WIDTH = 240
 def load_checkpoint(checkpoint, model):
     print('=> LOADING checkpoint.')
     model.load_state_dict(checkpoint["state_dict"])
+
+
+def segment_extraction(
+        original_image_path,
+        mask_image_path,
+        segment_image_path,
+        mode='foreground'
+):
+    inference_image_mask = np.array(Image.open(mask_image_path).convert("L"), dtype=np.float32)
+    inference_image_original = np.array(Image.open(original_image_path).convert("RGB"))
+    inference_image_mask = np.asarray(inference_image_mask / 255, dtype=np.uint8)
+
+    inference_image_original_masked = inference_image_original.copy()
+
+    # Mask blur for smoother images.
+    inference_image_mask = cv2.GaussianBlur(inference_image_mask, (17, 17), cv2.BORDER_DEFAULT)
+
+    if mode == 'foreground':
+        inference_image_original_masked[inference_image_mask == 0] = 0
+        inference_image_original_masked[inference_image_mask != 0] = inference_image_original[inference_image_mask != 0]
+    elif mode == 'background':
+        inference_image_original_masked[inference_image_mask != 0] = 0
+        inference_image_original_masked[inference_image_mask == 0] = inference_image_original[inference_image_mask == 0]
+
+    # import matplotlib.pyplot as plt
+    # plt.imshow(inference_image_mask)
+    # plt.show()
+
+    cv2.imwrite(segment_image_path, cv2.cvtColor(inference_image_original_masked, cv2.COLOR_RGB2BGR))
+
+    return inference_image_original_masked
 
 
 if __name__ == "__main__":
@@ -46,22 +80,22 @@ if __name__ == "__main__":
         # plt.imshow(image)
         # plt.show()
 
-        #image = loader(image).float()
         augmentations = test_transform(image=image)
         retimg = augmentations["image"].unsqueeze(0)  # this is for VGG, may not be needed for ResNet
-        #retimg = augmentations["image"].unsqueeze(1)
-        #return retimg  # assumes that you're using GPU
         return retimg.cuda()
 
 
-    aimage_path = image_loader(r'data/val_images/0ed6904e1004_12.jpg')
-    #aimage_path = image_loader(r'data/val_images/0cdf5b5d0ce1_12.jpg')
+    #aimage_path = image_loader(r'data/val_images/0ed6904e1004_12.jpg')
+    aimage_path = image_loader(r'data/val_images/0cdf5b5d0ce1_12.jpg')
     #aimage_path = image_loader(r'extra_images/6.jpg')
 
 
     folder = 'inference_results'
     inference_img_name = 'inference_img'
     inference_ori_img_name = 'inference_ori_img'
+    foreground_img_namge = 'inference_foreground_img'
+    background_img_namge = 'inference_background_img'
+    foreground_focus = 'foreground_focus_img'
 
     model.eval()
 
@@ -70,11 +104,36 @@ if __name__ == "__main__":
         preds = (preds > 0.5).float()
         #print(preds)
 
+
+    save_original_image_path = f"{folder}/inference_{inference_ori_img_name}.png"
+    save_mask_image_path = f"{folder}/inference_{inference_img_name}.png"
+    save_foreground_image_path = f"{folder}/inference_{foreground_img_namge}.png"
+    save_background_image_path = f"{folder}/inference_{background_img_namge}.png"
+    save_foreground_focus_image_path = f"{folder}/inference_{foreground_focus}.png"
+
+
     torchvision.utils.save_image(
-        preds, f"{folder}/inference_{inference_img_name}.png"
+        preds, save_mask_image_path
     )
 
     torchvision.utils.save_image(
-        aimage_path, f"{folder}/inference_{inference_ori_img_name}.png"
+        aimage_path, save_original_image_path
     )
 
+    foreground_image = segment_extraction(
+        original_image_path=save_original_image_path,
+        mask_image_path=save_mask_image_path,
+        segment_image_path=save_foreground_image_path,
+        mode='foreground',
+    )
+
+    background_image = segment_extraction(
+        original_image_path=save_original_image_path,
+        mask_image_path=save_mask_image_path,
+        segment_image_path=save_background_image_path,
+        mode='background',
+    )
+
+    # TODO: Smooth extracted foreground borders in saved images.
+    foreground_focused_image = cv2.blur(background_image, ksize=(8,8)) + foreground_image
+    cv2.imwrite(save_foreground_focus_image_path, cv2.cvtColor(foreground_focused_image, cv2.COLOR_RGB2BGR))
