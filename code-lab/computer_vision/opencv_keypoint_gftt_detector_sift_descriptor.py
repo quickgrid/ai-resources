b9730a9aae2933396b1936282c90aa7b6f354a48
Asif Ahmed <public.quickgrid@gmail.com>
2022-01-09 14:35:57 +0600

    Create opencv_keypoint_gftt_detector_sift_descriptor.py

---
+++ code-lab/computer_vision/opencv_keypoint_gftt_detector_sift_descriptor.py
@@ -0,0 +1,78 @@
+"""Opencv keypoint matching with GFTT keypoint detector and SIFT descriptor.
+
+This shows to use different algorithms for detector and descriptor in two steps
+instead of single step in detectAndCompute.
+"""
+
+import cv2
+import matplotlib.pyplot as plt
+import time
+
+img1 = cv2.imread(r'imgs/1.png', cv2.IMREAD_GRAYSCALE)
+img2 = cv2.imread(r'imgs/6.png', cv2.IMREAD_GRAYSCALE)
+
+start1 = time.time()
+
+#kp_detector = cv2.AKAZE_create()
+kp_detector = cv2.GFTTDetector_create()
+
+descriptor_extractor = cv2.SIFT_create()
+#descriptor_extractor = cv2.KAZE_create()
+
+
+keypoints1 = kp_detector.detect(img1)
+keypoints2 = kp_detector.detect(img2)
+
+print(keypoints1)
+print(keypoints2)
+
+keypoints1, descriptors1 = descriptor_extractor.compute(img1, keypoints1)
+keypoints2, descriptors2 = descriptor_extractor.compute(img2, keypoints2)
+
+# print(img1_kp)
+# print(img1_desc)
+
+# FLANN parameters
+FLANN_INDEX_KDTREE = 1
+
+index_params = dict(algorithm = FLANN_INDEX_KDTREE,
+                    trees = 5)
+
+search_params = dict(checks = 50)
+
+# Create FLANN object
+FLANN = cv2.FlannBasedMatcher(indexParams = index_params,
+                              searchParams = search_params)
+
+# Matching descriptor vectors using FLANN Matcher
+matches = FLANN.knnMatch(queryDescriptors = descriptors1,
+                         trainDescriptors = descriptors2,
+                         k = 2)
+
+# Lowe's ratio test
+ratio_thresh = 0.7
+
+# "Good" matches
+good_matches = []
+
+# Filter matches
+for m, n in matches:
+    if m.distance < ratio_thresh * n.distance:
+        good_matches.append(m)
+
+
+print("GOOD MATCHES", len(good_matches))
+
+end1 = time.time()
+print("FPS:", (1. / float(end1 - start1)))
+
+#img3 = cv2.drawMatchesKnn(img1,img1_kp,img2,img2_kp,matches,None,**draw_params)
+
+output = cv2.drawMatches(img1 = img1,
+                         keypoints1 = keypoints1,
+                         img2 = img2,
+                         keypoints2 = keypoints2,
+                         matches1to2 = good_matches,
+                         outImg = None,
+                         flags = cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+plt.imshow(output,),plt.show()
